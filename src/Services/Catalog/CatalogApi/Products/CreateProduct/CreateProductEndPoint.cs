62776b3cfa8e5f44cadac9494760d07de372d900
shawk <shawky1mohamed2@gmail.com>
2026-04-23 14:29:27 +0200

    Add Carter-based CreateProduct endpoint with Mapster mapping
    Refactored CreateProductEndPoint to use Carter for routing and Mapster for object mapping. Added request/response records, POST /products endpoint, and OpenAPI metadata. Updated project references to include Carter and Mapster.

--- src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductEndPoint.cs
+++ src/Services/Catalog/CatalogApi/Products/CreateProduct/CreateProductEndPoint.cs
@@ -1,6 +1,37 @@
-﻿namespace CatalogApi.Products.CreateProduct
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace CatalogApi.Products.CreateProduct
 {
-    public class CreateProductEndPoint
+
+    public record CreateProductRequest(
+        string Name,
+        string Description,
+        decimal Price,
+        List<string> Category,
+        string ImageFile
+    );
+    public record CreateProductResponse(Guid Id);
+    public class CreateProductEndPoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products", async (CreateProductRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateProductCommand>();
+                var res = await sender.Send(command);
+                var response = res.Adapt<CreateProductResponse>();
+                return Results.Created($"/products/{response.Id}",response);
+            }).WithName("CreateProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(statusCode: StatusCodes.Status400BadRequest)
+            .WithSummary("Create Product")
+            .WithDescription("Create a new product with the provided details.");
+
+
+
+
+        }
     }
 }
