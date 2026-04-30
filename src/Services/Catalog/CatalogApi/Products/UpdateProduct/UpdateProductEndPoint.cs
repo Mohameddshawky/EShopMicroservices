d8183c18087961725719d8e4a96a027f7b52f496
Adballahyouns <abdullahyounes.cs@gmail.com>
2026-04-30 13:35:50 +0300

    Add endpoint and handler for updating products
    Implemented a PUT /products endpoint with UpdateProductEndPoint and UpdateProductHandler. Handles product updates, returns success response, throws exception if product not found, and includes logging for traceability. Uses Marten for persistence.

---
+++ src/Services/Catalog/CatalogApi/Products/UpdateProduct/UpdateProductEndPoint.cs
@@ -0,0 +1,26 @@
+﻿using Carter;
+using Mapster;
+using MediatR;
+
+namespace CatalogApi.Products.UpdateProduct
+{
+    public record UpdateProductRequest(Guid Id, string Name, List<string> Category, string ImageFile, decimal Price);
+    public record UpdateProductResponse(bool IsSuccess);
+    public class UpdateProductEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPut("/products", async (UpdateProductRequest request, ISender sender) =>
+            {
+                var command =  request.Adapt<UpdateProductCommand>();
+                var res = await sender.Send(command);
+                var response =res.Adapt<UpdateProductResponse>();
+                return Results.Ok(response);
+            }).WithName("UpdateProduct")
+            .Produces<UpdateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithTags("Products")
+            .WithDescription("Update a product in the catalog");
+        }
+    }
+}
---
+++ src/Services/Catalog/CatalogApi/Products/UpdateProduct/UpdateProductHandler.cs
@@ -0,0 +1,32 @@
+﻿using BuildingBlocks.CQRS;
+using CatalogApi.Exceptions;
+using CatalogApi.Models;
+using Marten;
+
+namespace CatalogApi.Products.UpdateProduct
+{
+    public record UpdateProductCommand
+        (Guid Id, string Name, List<string> Category, string ImageFile, decimal Price):ICommand<UpdateProductResult>;
+    public record UpdateProductResult(bool IsSuccess);
+    internal class UpdateProductHandler(IDocumentSession session,ILogger<UpdateProductHandler> logger) : 
+        ICommandHandler<UpdateProductCommand, UpdateProductResult>
+    {
+        public async Task<UpdateProductResult> Handle(UpdateProductCommand request, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Handling UpdateProductCommand for product id: {Id}", request.Id);
+            var product = await session.LoadAsync<Product>(request.Id, cancellationToken);
+            if (product == null)
+            {
+                throw new ProductNotFoundException();
+            }
+            product.Name = request.Name;
+            product.Category = request.Category;
+            product.ImageFile = request.ImageFile;
+            product.Price = request.Price;
+            product.Category= request.Category;
+            session.Update(product);
+            await session.SaveChangesAsync(cancellationToken);
+            return new UpdateProductResult(true);
+        }
+    }
+}
