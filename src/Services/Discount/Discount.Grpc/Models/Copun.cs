6ccff62c7024bc0da6e87cb6652830edadda1f55
Mohameddshawky <shawky1mohamed2@gmail.com>
2026-05-12 14:19:25 +0300

    Start discount service

---
+++ src/Services/Discount/Discount.Grpc/Models/Copun.cs
@@ -0,0 +1,10 @@
+﻿namespace Discount.Grpc.Models
+{
+    public class Copun
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int Amount { get; set; }
+    }
+}
