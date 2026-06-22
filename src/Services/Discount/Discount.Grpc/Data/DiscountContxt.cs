066c501c2e5ba6f358edcb4177aa926b038e0eac
mohamed ahmed shawky <147840781+Mohameddshawky@users.noreply.github.com>
2026-06-22 18:03:50 +0200

    add data and sql lite

---
+++ src/Services/Discount/Discount.Grpc/Data/DiscountContxt.cs
@@ -0,0 +1,21 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContxt:DbContext
+    {
+        public DiscountContxt(DbContextOptions<DiscountContxt> options):base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Copun>().HasData(
+                new Copun { Id = 1, ProductName = "IPhone X", Description = "IPhone Discount", Amount = 150 },
+                new Copun { Id = 2, ProductName = "Samsung 10", Description = "Samsung Discount", Amount = 100 }
+                );
+        }
+        public DbSet<Copun> Copuns { get; set; }
+    }
+}
