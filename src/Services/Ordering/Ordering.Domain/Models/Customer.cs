5c6b61c4c99ada740ffc4f1a559ba60faf9a2911
mohamed ahmed shawky <147840781+Mohameddshawky@users.noreply.github.com>
2026-07-09 13:04:14 +0300

    complete domain

---
+++ src/Services/Ordering/Ordering.Domain/Models/Customer.cs
@@ -0,0 +1,25 @@
+﻿using Ordering.Domain.Abstractions;
+
+
+namespace Ordering.Domain.Models
+{
+    public class Customer:Entity<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public string Email { get; private set; } = default!;
+
+        // factory method
+        public static Customer Create(Guid customerId, string name, string email)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(name);
+            ArgumentException.ThrowIfNullOrWhiteSpace(email);
+            var customer = new Customer
+            {
+                Id = customerId,
+                Name = name,
+                Email = email
+            };
+            return customer;
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -0,0 +1,80 @@
+﻿using Ordering.Domain.Abstractions;
+using Ordering.Domain.Enums;
+using Ordering.Domain.Events;
+using Ordering.Domain.ValueObjects;
+
+
+namespace Ordering.Domain.Models
+{
+
+    //Aggregate root : Order => it is responsible for managing it's own state and it's assocciated order items
+    //Entity : OrderItem
+    //Value Object : Address, Money
+    public class Order : Aggregate<Guid>
+    {
+        private readonly List<OrderItem> _orderItems = new();
+        public IReadOnlyList<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public Guid CustomerId { get; private set; } = default!;
+        public string OrderName { get; private set; } = default!;
+
+        public Address ShippingAddress { get; private set; } = default!;
+        public Address BillingAddress { get; private set; } = default!;
+        public Payment Payment { get; private set; } = default!;
+        public OrderStatus Status { get; private set; } = OrderStatus.Pending;
+        public decimal TotalPrice
+        {
+            get
+            {
+                return _orderItems.Sum(item => item.Quantity * item.Price);
+            }
+            private set { }
+        }
+        public static Order Create(OrderId id, CustomerId customerId, OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment)
+        {
+            var order = new Order
+            {
+                Id = id,
+                CustomerId = customerId,
+                OrderName = orderName,
+                ShippingAddress = shippingAddress,
+                BillingAddress = billingAddress,
+                Payment = payment,
+                Status = OrderStatus.Pending
+            };
+
+            order.AddDomainEvent(new OrderCreatedEvent(order));
+
+            return order;
+        }
+
+        public void Update(OrderName orderName, Address shippingAddress, Address billingAddress, Payment payment, OrderStatus status)
+        {
+            OrderName = orderName;
+            ShippingAddress = shippingAddress;
+            BillingAddress = billingAddress;
+            Payment = payment;
+            Status = status;
+
+            AddDomainEvent(new OrderUpdatedEvent(this));
+        }
+
+        public void Add(ProductId productId, int quantity, decimal price)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            var orderItem = new OrderItem(Id, productId, quantity, price);
+            _orderItems.Add(orderItem);
+        }
+
+        public void Remove(ProductId productId)
+        {
+            var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+            if (orderItem is not null)
+            {
+                _orderItems.Remove(orderItem);
+            }
+        }
+    }
+}
