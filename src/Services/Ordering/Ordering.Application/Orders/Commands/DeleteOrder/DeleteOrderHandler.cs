2e79f707c8343ca19b3f071cd3758c2452ab5ffb
mohamed ahmed shawky <147840781+Mohameddshawky@users.noreply.github.com>
2026-07-15 14:48:55 +0300

    finish delete order command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderHandler.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderHandler(IAppDbContext dbContext)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand request, CancellationToken cancellationToken)
+        {
+           var orderId = OrderId.Of(request.Id);
+            var order = await dbContext.Orders.FindAsync(orderId, cancellationToken);
+            if (order == null)
+            {
+                throw new OrderNotFoundException(request.Id);
+            }
+            dbContext.Orders.Remove(order);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return new DeleteOrderResult(true);
+        }
+    }
+}
